322b74ed2caad5ac4e0d3b66cf9a4f1802a28de0
senti <brianmv@uci.edu>
2023-02-04 20:06:43 -0800

    done with login

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -4,6 +4,13 @@
 <header>
     <img src= "/favicon.png" alt= "logo" />
     <h1>UnderCover Boss</h1>
+    <nav class="bg-red-600 ml-auto p-4 rounded">
+      <ul class="flex gap-8">
+        <li class="hover:color-blue-500"><a href="/">Home</a></li>
+        <li class="hover:color-blue-500"><a href="/login">Login</a></li>
+        <li class="hover:color-blue-500"><a href="/register">Register</a></li>
+      </ul>
+    </nav>
 </header>
 
 
@@ -12,10 +19,13 @@
     header {
       display: flex;
       align-items: center;
+      background-color: rgb(65, 62, 62);
+    }
+
+    h1 {
       font-size: 48px;
       font-weight: bold;
       color: rgb(139, 139, 239);
-      background-color: rgb(65, 62, 62);
     }
 
     img {
